1c0f175e8f1fac2d71e548b7163650a747e719e9
sxsto <shitouer0515@outlook.com>
2021-09-29 00:12:44 +0800

    update

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "2021092701"
+var version = "2021092901"
 var describe = "常规"
 var AppName = "dmm"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
