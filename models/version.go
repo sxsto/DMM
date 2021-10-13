9d9f31488d8f605aaf236bc604f8c9f406706aba
sxsto <shitouer0515@outlook.com>
2021-10-13 22:07:33 +0800

    update 2021-10-13 22:00

--- models/version.go
+++ models/version.go
@@ -11,8 +11,8 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "2021100201"
-var describe = "QQ扫码???"
+var version = "2021101301"
+var describe = "局域网扫码"
 var AppName = "dmm"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
 
